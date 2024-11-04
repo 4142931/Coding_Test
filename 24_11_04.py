bd002dc33d467c54def8486f19803cfeb704f32e
KimYeongGwan <td950831@gmail.com>
2024-11-04 23:38:26 +0900

    n의 배수 고르기, 자릿수 더하기
    기초적인 문제로 쉽게 풀었다.

---
+++ 24_11_04.py
@@ -0,0 +1,31 @@
+# n의 배수 
+'''
+정수 n과 정수 배열 numlist가 매개변수로 주어질 때, 
+numlist에서 n의 배수가 아닌 수들을 제거한 배열을 
+return하도록 solution 함수를 완성해주세요.
+'''
+
+
+def solution(n, numlist):
+    answer = []
+    for i in numlist:
+        if i % n == 0:
+            answer.append(i)
+    return answer
+
+
+
+print(solution(5, [1, 9, 3, 10, 13, 5]))
+
+
+#자릿수 더하기 
+'''
+정수 n이 매개변수로 주어질 때 
+n의 각 자리 숫자의 합을 return하도록 
+solution 함수를 완성해주세요
+'''
+
+def solution(n):
+    return sum(list(map(int, str(n))))
+
+print(solution(1234))
