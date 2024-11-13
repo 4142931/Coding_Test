bf940f1d46afe492123e9acbe57d462a201ffaa8
KimYeongGwan <td950831@gmail.com>
2024-11-13 20:10:05 +0900

    캐릭터 좌표

---
+++ 24_11_13.py
@@ -0,0 +1,41 @@
+#캐릭터 좌표
+
+'''
+머쓱이는 RPG게임을 하고 있습니다. 
+게임에는 up, down, left, right 방향키가 있으며 
+각 키를 누르면 위, 아래, 왼쪽, 오른쪽으로 한 칸씩 이동합니다. 
+예를 들어 [0,0]에서 up을 누른다면 캐릭터의 좌표는 
+[0, 1], down을 누른다면 [0, -1], left를 누른다면 
+[-1, 0], right를 누른다면 [1, 0]입니다. 
+머쓱이가 입력한 방향키의 배열 keyinput와 
+맵의 크기 board이 매개변수로 주어집니다. 
+캐릭터는 항상 [0,0]에서 시작할 때 키 입력이 
+모두 끝난 뒤에 캐릭터의 좌표 [x, y]를 return하도록 
+solution 함수를 완성해주세요.
+[0, 0]은 board의 정 중앙에 위치합니다. 
+예를 들어 board의 가로 크기가 9라면 
+캐릭터는 왼쪽으로 최대 [-4, 0]까지 
+오른쪽으로 최대 [4, 0]까지 이동할 수 있습니다.
+'''
+
+# 1. keyinput에 들어가는 입력 값에 따라 점의 위치가 변경되어야 한다.
+# 2. 맵의 크기인 board의 값에 따라 최대치가 정해진다
+
+def solution(keyinput, board):
+    answer = [0, 0]
+    x_limit = board[0] // 2
+    y_limit = board[1] // 2
+
+    for i in keyinput:
+        if i == "left" and answer[0] > -x_limit:
+            answer[0] -= 1
+        elif i == "right" and answer[0] < x_limit:
+            answer[0] += 1
+        elif i == "down" and answer[1] > -y_limit:
+            answer[1] -= 1
+        elif i == "up" and answer[1] < y_limit:
+            answer[1] += 1
+
+    return answer
+
+print(solution(["down", "down", "down", "down", "down"], [7, 9]))
